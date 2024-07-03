3ad9779fc1206e25ce19734293847ad60c195895
Rafael dos Santos Giorgi <106035150+rafaelnator@users.noreply.github.com>
2024-07-03 14:09:22 -0300

    Back-end-v4

--- back-end/src/routes.ts
+++ back-end/src/routes.ts
@@ -1,9 +1,16 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 
 import { CreateUserControllers } from './controllers/user/CreateUserControllers';
+import { AuthUserControllers } from './controllers/user/AuthUserControllers';
 
 const router = Router();
 
+router.get('/ping', (req: Request, res: Response) => {
+    return res.json({ pong: true });
+});
+
 router.post('/users', new CreateUserControllers().handle);
+
+router.post('/session', new AuthUserControllers().handle);
 
 export { router };
