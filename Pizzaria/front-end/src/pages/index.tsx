039fde9bd20495763c5d6939256214c70274c1ba
Rafael dos Santos Giorgi <106035150+rafaelnator@users.noreply.github.com>
2024-08-02 10:58:30 -0300

    Front-end-v2

--- Pizzaria/front-end/src/pages/index.tsx
+++ Pizzaria/front-end/src/pages/index.tsx
@@ -1,13 +1,36 @@
+import { useContext, FormEvent, useState } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import style from "../../styles/home.module.scss";
 
-import logoImg from '../../public/logo_Completo.png'
+import logoImg from '../../public/logo_Completa.png'
 
 import { Input } from "../components/ui/Input";
 import { Button } from "../components/ui/Button";
 
+import { AuthContext } from "../contexts/AuthContext";
+
+import Link from "next/link";
+
 export default function Home() {
+  const { singIn } = useContext(AuthContext);
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [loading, setLoading] = useState(false);
+
+  async function handleLogin(event: FormEvent) {
+    event.preventDefault();
+
+    let data = {
+      email,
+      password
+    }
+
+    await singIn(data);
+  }
+
   return (
     <>
       <Head>
@@ -18,17 +41,32 @@ export default function Home() {
         <Image className={style.logoImg} src={logoImg} alt="Logo Nome Don Nápoli Pizzaria"></Image>
 
         <div className={style.login}>
-          <form>
-            <Input placeholder="Digite seu email" type="text"/>
+          <form onSubmit={handleLogin}>
+            <Input 
+              placeholder="Digite seu email" 
+              type="text"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+            />
             
-            <Input placeholder="Digite sua senha" type="password"/>
+            <Input 
+              placeholder="Digite sua senha" 
+              type="password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+            />
 
-            <Button type="submit" loading={false}>
+            <Button 
+              type="submit" 
+              loading={false}
+            >
               Acessar
             </Button>
           </form>
 
-          <a className={style.text}>Não possui uma conta? Cadastre-se</a>
+          <Link className={style.text} href="/signup">
+            Não possui uma conta? Cadastre-se
+          </Link>
         </div>
       </div>
     </>
