be230dbb91035573e7b7780fd881b6e18893f9d1
Rafael dos Santos Giorgi <106035150+devraffles@users.noreply.github.com>
2024-10-09 15:36:37 -0300

    Front-end-v2
    Adição de Cadastro, Funcionalidade de Login e Cadastro, verificação de token e logon

--- Pizzaria/frontend/src/app/page.tsx
+++ Pizzaria/frontend/src/app/page.tsx
@@ -3,8 +3,52 @@ import logoimg from '/public/logo-Nome-white.svg'
 
 import Image from "next/image";
 import Link from "next/link";
+import { redirect } from "next/navigation";
+
+import { api } from "@/services/api";
+import { cookies } from "next/headers";
 
 export default function Page() {
+
+  async function handleLogin(formData: FormData){
+    "use server"
+
+    const email = formData.get("email")
+    const password = formData.get("password")
+
+    if(email === "" || password === ""){
+      return;
+    }
+
+    try {
+      const response = await api.post("/session", {
+        email,
+        password
+      })
+
+      if(!response.data.token){
+        return;
+      }
+
+      console.log(response.data);
+
+      const expressTime = 60 * 60 * 24 * 30 * 1000
+      cookies().set("session", response.data.token, {
+        maxAge: expressTime,
+        path: "/",
+        httpOnly: false,
+        secure: process.env.NODE_ENV === "production"
+      })
+
+    } catch (error) {
+      console.log("erro")
+      console.log(error)
+      return;
+    }
+
+    redirect("/dashboard")
+  }
+
  return (
    <>
     <div className={style.containerCenter}>
@@ -15,7 +59,7 @@ export default function Page() {
       />
 
       <section className={style.login}>
-        <form>
+        <form action={handleLogin}>
           <input 
             type="email" 
             required
